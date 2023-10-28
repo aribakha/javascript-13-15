466088fcc97f8ea13d1019f30b8dcb46f4dd23ff
aribakha <140427904+aribakha@users.noreply.github.com>
2023-10-28 21:35:58 +0500

    JS assignment

---
+++ app.js
@@ -0,0 +1,111 @@
+                                                  // CHAPTER # 14 (ARRAY)
+
+                                                      // Qusetion # 1
+
+// var studentName = {}
+// studentName.push("areeba");
+// studentName.push("hafsa");
+
+                                                      // Qusetion # 2
+
+// var studentName = {}
+// studentName.push[0] = "areeba";
+// studentName.push[0] = "hafsa";
+
+                                                      // Qusetion # 3
+
+                                                     //"string array"   
+
+
+// var arr = ["cat","dog","lion","elephant"]
+// document.write(arr)                                                      
+
+                                                      // Qusetion # 4
+
+                                                     //"number array"   
+
+// var arr = [1,2,3,4,5,6,7,8,9,10]
+// document.write(arr)
+
+                                                      // Qusetion # 5
+
+                                                     //"boolean array"   
+
+// var arr = [true,false,true,false]
+// document.write(arr)
+
+                                                      // Qusetion # 6
+
+                                                     //"mixed array"   
+
+// var arr = ["cat","dog",1,2,3,true,false,]
+// document.write(arr)
+
+                                                      // Qusetion # 7
+
+// var qualifications = ["1) SSC","2) HSC","3) BSC","4) BS","4) BCOM","6) MS","7) M.PHIL","8) PHD"]  
+// var i;
+// for (i = 0;i < qualifications.length; i++){
+// document.write(qualifications[i] +"<br/>")                                                 
+// }
+                                                      // Qusetion # 8
+
+// var studentArr = ["Michael","John","Tony"];
+// var numberArr = [320,230,480];
+// var total = 500;
+// var michaelPer = numberArr[0]*100/total; 
+// var johnPer =  numberArr[1]*100/total;
+// var tonyPer =  numberArr[2]*100/total;
+// document.write("Score of Michael is " + numberArr[0] +  " Percentage : "  + michaelPer+"%" +"</br>")
+// document.write("Score of john is " + numberArr[1] +  " Percentage : "  + johnPer+"%" +"</br>")
+// document.write("Score of Tony is " + numberArr[2] +  " Percentage : "  + tonyPer+"%" +"</br>")
+
+                                                      // Qusetion # 
+                                                      
+// var studentScore = prompt("Enter a any color name");                                                     
+// var colorName = [" red ", " yellow "," green "," blue "," brown "];
+// colorName.unshift(" purple ");
+// colorName.push("orange");
+// colorName.unshift("sky blue","light green");
+// colorName.shift("sky blue");
+// colorName.pop("orange");
+// colorName.splice(4,0, "lemon");
+// colorName.shift(0);
+// colorName.splice(3,1);
+// document.write("Color name : " + "</br>" + colorName)
+
+                                                      // Qusetion # 10
+
+// var studentScore =[320,230,480,120];
+// var orderedScore = [studentScore[3],studentScore[1],studentScore[0],studentScore[2]];
+// document.write("Scores of students : " + studentScore + "</br>");
+// document.write("Ordered scores of students : " + orderedScore)
+
+                                                      // Qusetion # 11
+
+// var citiesList = ["Karachi","Lahore","Islamabad","Quetta","Peshawar"];
+// var selectedCities = [citiesList[0],citiesList[2],citiesList[1]];
+// document.write("Cities list : " + citiesList + "</br>");
+// document.write("Selected cities list : " + selectedCities)
+
+                                                      // Qusetion # 12
+
+// var arr = ["This","is","my","cat"];
+// var joinArr = arr.join(" ");
+// document.write("Array : " +arr +"</br>");
+// document.write("String : " + joinArr);
+
+                                                      // Qusetion # 13
+
+// var devices = ["Keyboard","mouse","printer","monitor"];
+// document.write("Devices : " + devices + "</br>")
+// for (var i = 0; i < devices.length; i++){
+//     document.write("Out : " +"</br>" + devices[i] + "</br>")
+// }
+
+                                                      // Qusetion # 14
+var devices = ["Keyboard","mouse","printer","monitor"];
+document.write("Devices : " + devices + "</br>")
+for (var i = 0; i < devices.length; i++){
+document.write("Out : " +"</br>" + devices[i] + "</br>")
+  }                                                     
